fe847d179a6adc5be0f91b7d0f72fa5c9250f85e
Chris <kitecabrinha@gmail.com>
2017-08-08 11:49:43 -0400

    adding security and login

--- src/main/java/com/example/customer/MainController.java
+++ src/main/java/com/example/customer/MainController.java
@@ -4,6 +4,7 @@ import com.example.customer.service.CustomerService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -43,5 +44,9 @@ public class MainController {
             customer.setPhone(phone);
             customerService.add(customer);
             return "redirect:/Customers";
+    }
+    @GetMapping("/login")
+    String login() {
+        return "login";
     }
 }
