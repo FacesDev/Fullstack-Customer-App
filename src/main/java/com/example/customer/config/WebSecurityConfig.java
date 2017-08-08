c5c9786f9657e6677a735faaabf05dbec1c76d1f
Chris <kitecabrinha@gmail.com>
2017-08-08 12:36:12 -0400

    debugging

--- src/main/java/com/example/customer/config/WebSecurityConfig.java
+++ src/main/java/com/example/customer/config/WebSecurityConfig.java
@@ -7,15 +7,21 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
+import javax.sql.DataSource;
+
 @Configuration
 @EnableWebSecurity
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Autowired
+    private DataSource dataSource;
+
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         // @formatter:off
         http
                 .authorizeRequests()
-                .antMatchers("/", "/user").permitAll()
+                .antMatchers("/", "/Customers").permitAll()
                 .anyRequest().authenticated()
                 .and()
                 .formLogin()
@@ -29,8 +35,8 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Autowired
     public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
-        auth
-                .inMemoryAuthentication()
-                .withUser("user").password("password").roles("USER");
+        auth.jdbcAuthentication().dataSource(this.dataSource)
+                .usersByUsernameQuery("select username, password, enabled from users where username=?")
+                .authoritiesByUsernameQuery("select username, authority from authorities where username = ?");
     }
 }
